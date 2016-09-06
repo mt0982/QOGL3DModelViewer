a4033525b8ae8296a27ca523dcfb2f1fedc9596c
mt0982 <tulikowski.maciej@gmail.com>
2016-09-06 16:01:59 +0200

    ModelLoader

--- Loader/modelloader.cpp
+++ Loader/modelloader.cpp
@@ -4,6 +4,8 @@ void parse(QVector<QVector3D> &vertices, QVector<QVector3D> &normals, QVector<ui
 {
     /* Temporary Variable */
     QVector<QVector3D> temp_normals;
+    QVector<QVector2D> temp_uv;
+    QVector<uint> normalIndices;
 
     /* File */
     ifstream in("/home/asus/Pulpit/untitled3.obj", ios::in);
@@ -25,7 +27,14 @@ void parse(QVector<QVector3D> &vertices, QVector<QVector3D> &normals, QVector<ui
         else if (line.substr(0,3) == "vn ") {
             istringstream s(line.substr(2));
             QVector3D n; s >> n[0]; s >> n[1]; s >> n[2];
-            normals.push_back(n);
+            temp_normals.push_back(n);
+        }
+
+        /* UV */
+        else if (line.substr(0,3) == "vt ") {
+            istringstream s(line.substr(2));
+            QVector2D n; s >> n[0]; s >> n[1];
+            temp_uv.push_back(n);
         }
 
         /* Indices */
@@ -35,16 +44,30 @@ void parse(QVector<QVector3D> &vertices, QVector<QVector3D> &normals, QVector<ui
             const char *chh = line.c_str();
             sscanf(chh, "f %d/%d/%d %d/%d/%d %d/%d/%d", &vx, &uvx, &nx, &vy, &uvy, &ny, &vz, &uvz, &nz);
 
-            /* Important. Blender indexing from 0 */
-            vx--; vy--; vz--;
+            /* Important. Blender indexing from 1 */
+            vx--;  vy--;   vz--;
+            nx--;  ny--;   nz--;
+            uvx--; uvy--;  uvz--;
 
+            /* Vertex Indices */
             indices.push_back(vx);
             indices.push_back(vy);
             indices.push_back(vz);
+
+            /* Normal Indices */
+            normalIndices.push_back(nx);
+            normalIndices.push_back(ny);
+            normalIndices.push_back(nz);
         }
     }
 
-    qDebug() << vertices.size() << indices.size();
+    /* Processing Data */
+    for(int i = 0; i < normalIndices.size(); i++) {
+        uint index = normalIndices[i];
+        normals.push_back(temp_normals[index]);
+    }
+
+    qDebug() << vertices.size(); //temp_uv.size();
 }
 
 ModelLoader::ModelLoader()
@@ -65,15 +88,17 @@ void ModelLoader::init(QOpenGLShaderProgram &program)
     //model.parseOBJ(vertices, indices);
     parse(vertices, normals, indices);
 
+    qDebug() << vertices.size() << indices.size() << normals.size();
+
     /* Colors */
     for(int i = 0; i < vertices.size(); i++) {
         colors.push_back(QVector3D(0.3, 0.4, 0.35));
     }
 
     /* Normals */
-    normals.clear();
+    /*normals.clear();
     normals.resize(vertices.size());
-    model.calculateNormals(vertices, normals, indices);
+    model.calculateNormals(vertices, normals, indices);*/
 
     /* Set Attribute */
     model.init();
